f4ac720634aecf899e99c96b90bb6f4f23f15a9a
Hemant kr <hk9051640@gmail.com>
2022-03-12 10:47:29 +0530

    "userRoutes and userModels creations'

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const port =  5001
+const port =  process.env.port || 5000
 const dbConnection = require('./db')
-
+app.use(express.json())
 
 
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
+app.use('/api/users/' , require('./routes/usersRoute'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
